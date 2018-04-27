8cb96b3aadd5fb8b627208488d38790341ece0fb
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-04-27 13:48:49 -0600

    it works

--- beerMe/app.js
+++ beerMe/app.js
@@ -1,6 +1,7 @@
 var $clickYourDay = document.querySelectorAll('.your-day')
 var $picDiv = document.querySelector('.allPics')
 var $main = document.querySelector('main')
+$main.classList = 'beer-main'
 let apiUrl = ''
 
 
@@ -8,7 +9,6 @@ $clickYourDay.forEach((meme)=> {
   meme.addEventListener('click', function () {
     var clickedId = event.target.id
     makeQueryString(clickedId)
-    console.log(apiUrl)
     hideClickedElement()
     showResetButton()
     getBeer()
@@ -16,13 +16,11 @@ $clickYourDay.forEach((meme)=> {
 })
 
 function getBeer (){
-  console.log(apiUrl)
 fetch(apiUrl)
   .then(function(response) {
     return response.json()
   })
   .then(function(data) {
-    console.log(data);
     var selectedBeer = getRandomNumber(data)
     createDiv(selectedBeer)
 
@@ -37,15 +35,17 @@ fetch(apiUrl)
   }
 
   function showResetButton() {
-    var $resetButton = document.createElement('button')
+    var $resetButton = document.createElement('a')
     $resetButton.classList = 'reset-button'
+    $resetButton.textContent = 'Have Another'
+    $resetButton.href = "characters.html"
     $main.appendChild($resetButton)
   }
 
   function makeQueryString(clickedId){
     switch (clickedId) {
       case '1':
-        apiUrl = 'https://api.punkapi.com/v2/beers?abv_lt=4.1'
+        apiUrl = 'https://api.punkapi.com/v2/beers?abv_lt=4'
         break;
       case '2':
         apiUrl = 'https://api.punkapi.com/v2/beers?abv_lt=8&abv_gt=4'
@@ -61,20 +61,42 @@ fetch(apiUrl)
         break;
       default:
       console.log('nope');
-
     }
     return apiUrl
   }
 
   function createDiv(beer){
+    
     var $aboveButton = document.querySelector('.reset-button')
     var $beerDiv = document.createElement('div')
     $beerDiv.classList = 'your-beer'
-    $beerDiv.innerHTML = "<img src= `${beer.img_url}`> <h3>`${beer.name}`</h3><p>`${beer.abv}`</p><p>`${beer.description}`</p>"
     $main.appendChild($beerDiv)
-
+    createImg(beer, $beerDiv)
+    createH3(beer, $beerDiv)
+    createP1(beer, $beerDiv)
+    createP2(beer, $beerDiv)
   }
+function createImg(beer, $parent) {
+  var $img = document.createElement('img')
 
+  $img.src = beer.image_url
+  $parent.appendChild($img)
+}
+function createH3(beer, $parent) {
+  var $h3 = document.createElement('h3')
+  $h3.textContent = beer.name
+  $parent.appendChild($h3)
+}
+function createP1(beer, $parent) {
+  var $p1 = document.createElement('p')
+  $p1.textContent = beer.abv
+  $parent.appendChild($p1)
+}
+function createP2(beer, $parent) {
+  var $p2 = document.createElement('p')
+  $p2.textContent = beer.description
+  $parent.appendChild($p2)
+}
   function getRandomNumber(data) {
     var lengthOfArray = data.length
     var randomBeerSelection = Math.floor(Math.random() * lengthOfArray)
